fa7d4f61b0c66924aca95026464d8a4b0e7fa625
Jenn Schiffer <jenn@pancaketheorem.com>
2013-10-22 23:11:08 -0400

    change feature to header in js

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,7 +9,7 @@
     $(document).ready(function(){
 
         // set background image of post header
-        var $postImage = $('#feature-img');
+        var $postImage = $('#header-img');
         if ( $postImage ) {
 	        var postImageURL = $postImage.attr('src');
 	        $('#site-head').css('background-image',postImageURL);
